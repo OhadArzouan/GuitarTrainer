d384149b4a4a52de266ea0e773cbe15b8cebcf3c
OhadArzouan <ohad.arzouan@gmail.com>
2025-08-09 21:38:58 +0200

    Initial Commit

---
+++ GuitarTrainer/GuitarTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTrainerApp.swift
+//  GuitarTrainer
+//
+//  Created by Ohad on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
